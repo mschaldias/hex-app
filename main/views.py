80120924c8709ac700c3ae19664384f0fcca7962
mschaldias <msdias.blue@gmail.com>
2023-04-04 22:47:50 -0700

    removed old comments

--- main/views.py
+++ main/views.py
@@ -6,8 +6,6 @@ from .forms import CreateNewList
 # Create your views here.
 
 def index(request, id):
-    # item = ls.item_set.get(id = 1) 
-    # return HttpResponse("%s<br></br><p>%s</p>" %(ls.name, item.text))
 
     ls = request.user.todolist_set.filter(id=id).first()
 
@@ -24,7 +22,6 @@ def index(request, id):
 
         if request.method == "POST":
             print(request.POST)
-            # if request.POST.get("save"):
             for item in ls.item_set.all():
                 if request.POST.get("c" + str(item.id)) == "clicked":
                     item.complete = True
