b5c8948ddbef89ebc7fdf722be24ff2c0e562044
mschaldias <msdias.blue@gmail.com>
2023-04-09 12:33:35 -0700

    refactor, style changes, button functionality
    update remove action name
    
    implemented edit functionality, changed remove_button name to convention
    
    new button styles
    
    implemented add item button
    
    updated btn-outline-hex-light style
    
    moved list_item assignment after ajax
    
    updated variable names, added list_id arg to edit_text
    
    fix indentation, variable names
    
    add list_id argument to edit_text call
    
    update col div and spacing
    
    add_button creates new item with backend id
    
    remove alert

--- main/static/js/item_events.js
+++ main/static/js/item_events.js
@@ -1,10 +1,28 @@
+
+function edit_text(item_id,value){
+    $.ajax({
+        type:'POST',
+        url: "/item_actions/",
+        data: {
+            csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val(),
+            item_id: item_id,
+            action: "edit",
+            text: value
+        },
+        // success:function(){
+        //     alert('success');
+        // }                        
+    });
+};
+
+
 function checkbox_click(item_id){
     $.ajax({
         type:'POST',
         url: "/item_actions/",
         data: {
             csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val(),
-            'id': item_id,
+            item_id: item_id,
             action: "checkbox"
         },
         // success:function(){
@@ -14,6 +32,7 @@ function checkbox_click(item_id){
 };
 
 function remove_button(item_id){
+    var item_id = "item" + item_id.replace(/\D/g, "");
     const element = document.getElementById(item_id);
     element.remove();
     $.ajax({
@@ -21,13 +40,54 @@ function remove_button(item_id){
         url: "/item_actions/",
         data: {
             csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val(),
-            'id': item_id,
-            action: 'remove-item'
+            item_id: item_id,
+            action: 'remove'
         },
         // success:function(){
         //     alert('success');
         // }                        
     });
+};
+
+function append_new_item(list_id,item_id){
+var list_item = 
+    `<li class="list-group-item list-group-item-action list-group-item-hex text-white draggable" id = "item` + item_id + `}">
+        <div class = "input-group mx-auto">
+            <span class="handle btn border-0" style= "cursor:move"><i class="fa-solid fa-grip-lines-vertical fa" style="color:  var(--hex-body);"></i></span>
+                <div class= "input-group-text bg-hex-sidenav border border-hex" >
+                    <input class = "form-check-input bg-hex-topnav" type="checkbox", value = "clicked", name = "click` + item_id + `", onclick="checkbox_click(name)">
+                </div> 
+            <textarea id = "textarea" value = "" name = "edit` + item_id + `" class = "form-control bg-hex-topnav border border-hex text-white" onchange="edit_text(name,value)"></textarea>
+            <button  id = "remove_button", name = "remove` + item_id + `", value = "` + item_id + `", class="btn btn-outline-hex-dark", onclick = "remove_button(name)"><i class="fa fa-minus"></i></button>
+        </div>
+    </li>`
+
+    list_id="#" + list_id
+    item_clone = $(list_id)
+
+    $(list_id).append(list_item);
+}
+
+function add_button(list_id){
+    
+    $.ajax({
+        type:'POST',
+        url: "/item_actions/",
+        data: {
+            csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val(),
+            sortable: true,
+            action: 'add',
+            list_id: list_id,
+            item_ids: []
+        },
+        success: (data) => {
+            append_new_item(list_id,data)
+        },
+        error: (error) =>{
+            console.log(error);
+        }                        
+    });
+
 };
 
 function sortable_event(action,list_id){
@@ -42,10 +102,10 @@ function sortable_event(action,list_id){
         url: "/item_actions/",
         data: {
             csrfmiddlewaretoken:$('input[name=csrfmiddlewaretoken]').val(),
-            'sortable': true,
-            'action': action,
-            'list-id': list_id,
-            'item-ids': ids_list
+            sortable: true,
+            action: action,
+            list_id: list_id,
+            item_ids: ids_list
         },
         // success:function(){
         //     alert('success');
--- main/views.py
+++ main/views.py
@@ -6,6 +6,8 @@ from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_GET,require_POST
 # Create your views here.
 
+MAX_ITEMS = 1000
+
 @login_required(login_url='/login/')
 def index(request, id):
 
@@ -74,30 +76,44 @@ def item_actions(request):
     if request.method == "POST":
         print(request.POST)
         if request.POST.get("sortable"):
-                list_id = ''.join([n for n in request.POST.get("list-id") if n.isdigit()])
-                item_ids = [''.join([n for n in i if n.isdigit()]) for i in request.POST.getlist("item-ids[]") if i]          
-                ls = request.user.todolist_set.filter(id=list_id).first()
-                
-                if request.POST.get("action") == "move":
-                    for id in item_ids:
-                        item = Item.objects.all().filter(id=id).first()
-                        item.todolist = ls
-                        item.save()
-                
+            action = request.POST.get("action")
+            list_id = ''.join([n for n in request.POST.get("list_id") if n.isdigit()])
+            item_ids = [''.join([n for n in i if n.isdigit()]) for i in request.POST.getlist("item_ids[]") if i]          
+            ls = request.user.todolist_set.filter(id=list_id).first()
+
+            if action == "add":
                 position = 0
+                for item in ls.item_set.all():
+                    position = item.position
+                new_item = ls.item_set.create(complete = False, position = position+1)
+                return HttpResponse(new_item.id)
+
+            
+            elif action == "move":
                 for id in item_ids:
-                    item = Item.objects.all().filter(id=id,todolist__id=ls.id).first()
-                    item.position = position
+                    item = Item.objects.all().filter(id=id).first()
+                    item.todolist = ls
                     item.save()
-                    position+=1
+
+            #update list position
+            position = 0
+            for id in item_ids:
+                item = Item.objects.all().filter(id=id,todolist__id=ls.id).first()
+                item.position = position
+                item.save()
+                position+=1
 
         elif request.POST.get("action"): 
             action = request.POST.get("action")
-            id = ''.join([n for n in request.POST.get("id") if n.isdigit()])
+            id = ''.join([n for n in request.POST.get("item_id") if n.isdigit()])           
             item = Item.objects.all().filter(id=id,todolist__user=request.user).first()
             if item:
                 if action == "remove":
                     item.delete()
+                elif action == "edit":
+                    item.text = request.POST.get("text")
+                    item.save()
+
                 elif action == "checkbox":
                     item.complete = not(item.complete)
                     item.save()
