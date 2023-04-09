ce0adc407ebbfe7b9b61e9340d5b8a7b77afc3e6
mschaldias <msdias.blue@gmail.com>
2023-04-08 22:21:11 -0700

    added missing bracket in string

--- main/static/js/sortable_event.js
+++ main/static/js/sortable_event.js
@@ -1,7 +1,7 @@
 
 function sortable_event(action,list_id){
 
-    let ids = document.querySelectorAll( "#" + list_id + " li[id");
+    let ids = document.querySelectorAll( "#" + list_id + " li[id]");
     let ids_list = [];
     for (let i = 0; i < ids.length; i++) {
         ids_list.push(ids[i].id);
