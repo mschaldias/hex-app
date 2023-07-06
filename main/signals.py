2cf8a444697b07a58c861cf5e69ba6d073a2ce86
Mariana <msdias.blue@gmail.com>
2023-07-05 17:53:17 -0700

    signal for profile and board hexlog

--- main/signals.py
+++ main/signals.py
@@ -1,7 +1,7 @@
 from django.db.models.signals import post_save, pre_delete
 from django.contrib.auth.models import User
 from django.dispatch import receiver
-from .models import Board,ToDoList 
+from .models import Board, Profile,ToDoList 
  
 @receiver(post_save, sender=User)
 def create_board(sender, instance, created, **kwargs):
@@ -10,6 +10,12 @@ def create_board(sender, instance, created, **kwargs):
         board.todolist_set.create(name="archive")
         board.todolist_set.create(name="backlog")
         board.todolist_set.create(name="futurelog")
+        board.todolist_set.create(name="hexlog")
+        board.hexable = True
         board.initialize_week()
 
   
+@receiver(post_save, sender=User)
+def create_profile(sender, instance, created, **kwargs):
+    if created:
+        Profile.objects.get_or_create(owner=instance)
